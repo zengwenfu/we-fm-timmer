808711e9b7c147701e443e310bf82fbc848c807b
zengwenfu836 <zengwenfu836@pingan.com.cn>
2017-04-10 17:43:43 +0800

    修改登录机制

--- util/loginUtil.js
+++ util/loginUtil.js
@@ -8,42 +8,45 @@
  */
 function login() {
     return new Promise(function(resolve, reject) {
-        wx.login({
-            success: function(res) {
-                 wx.login({
-                    success: function(res) {
-                        //发起网络请求
-                        wx.request({
-                            url: 'https://we.facemagic888.com/user/login',
-                            method: 'POST',
-                            data: {
-                                code: res.code
-                            },
-                            success: function(result) {
-                                result = result.data;
-                                if(result.code === '0') {
-                                    wx.setStorage({
-                                        key: 'sessionid',
-                                        data: result.data.sessionid,
-                                        success: function(res){
-                                            // success
-                                        },
-                                        fail: function() {
-                                            // fail
-                                        },
-                                        complete: function() {
-                                            // complete
-                                        }
-                                    });
-                                    resolve(result.data.sessionid);
-                                }
-                                resolve();
-                            }
-                        });
+        //读取本地session_id
+        getSessionId().then(function(sessionid) {
+            wx.login({
+                success: function(res) {
+                    var param = {
+                        code: res.code
+                    };
+                    if(sessionid) {
+                        param.sessionid = sessionid;
                     }
-                 });
-            }
-        });
+                    //发起网络请求
+                    wx.request({
+                        url: 'https://we.facemagic888.com/user/login',
+                        method: 'POST',
+                        data: param,
+                        success: function(result) {
+                            result = result.data;
+                            if(result.code === '0') {
+                                wx.setStorage({
+                                    key: 'sessionid',
+                                    data: result.data.sessionid,
+                                    success: function(res){
+                                        // success
+                                    },
+                                    fail: function() {
+                                        // fail
+                                    },
+                                    complete: function() {
+                                        // complete
+                                    }
+                                });
+                                resolve(result.data);
+                            }
+                            resolve();
+                        }
+                    });
+                }
+            });
+        })
     });
 }
 
@@ -51,7 +54,7 @@ function login() {
  *  本地是否保存过session_id，如果有的话，即使会话过期，也不为用户重新注册了
  *  autoLogin，拿不到session的时候是否需要自动登录
  */
-function getSessionId(autoLogin = true) {
+function getSessionId() {
     return new Promise(function(resolve, reject) {
         wx.getStorage({
           key: 'sessionid',
@@ -59,17 +62,7 @@ function getSessionId(autoLogin = true) {
                 resolve(res.data);
           },
           fail: function() {
-            if(autoLogin) {
-                //登录 如果有必要的话注册
-                login().then(function(sessionid) {
-                    if(sessionid) {
-                        regist(sessionid);
-                    }
-                    resolve(sessionid);
-                });
-            } else {
-                resolve(false);
-            }
+            resolve(false);
           }
         })
     });
@@ -123,20 +116,14 @@ function checkSession() {
                 resolve();
             },
             fail: function() {
-                // 先看以前有没有登录过
-                getSessionId(false).then(function(res) {
-                    var hasRegisted = false;
-                    if(res) {
-                        hasRegisted = true;
+                //登录 如果有必要的话注册
+                login().then(function(data) {
+                    //服务器会通知这个用户有否注册过
+                    if(data.registered === '0') {
+                        regist(data.sessionid);
                     }
-                    //登录 如果有必要的话注册
-                    login().then(function(sessionid) {
-                        if(!hasRegisted && sessionid) {
-                            regist(sessionid);
-                        }
-                        resolve();
-                    });
-                })
+                    resolve();
+                });
             }
         });
     });
